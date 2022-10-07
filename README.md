98db1c369ce7b0a4ace190771f9f7caa4ca84169
Ethan Fidler <ethancf1999@gmail.com>
2022-10-07 09:40:23 -0400

    faucet page + buttons

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CS5167
+
 projects for CS5167 - User Interfaces I
+
+## TODO still
+
+-   add music page
+-   add profiles page
+-   add timer page
+-   make top bar work
+-   stop/start button functionality
--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 let temperatureInput = document.getElementById("temp-input");
 let temperatureLabel = document.getElementById("temp-label");
 
+let buttonIDs = [
+    "jetPatternButton",
+    "showerPatternButton",
+    "sprayPatternButton",
+    "mistPatternButton",
+    "pulsePatternButton",
+];
+
+let pageIds = ["main-page", "music-page", "faucet-page"];
+
+// MAIN DATA (will be refactored into a data structure)
+
 let temperatureValue = 85;
 let pressureValue = 50;
+let sprayPatternValue = "shower";
+let activeProfile = "Ethan Fidler";
+let timeRemaining = "0";
 
-// move gradient
+// move gradient on main page slider
 temperatureInput.addEventListener("input", function () {
     //Change slide thumb color on way up 17.5 intervals
     if (this.value > 68) {
@@ -39,8 +54,33 @@ temperatureInput.addEventListener("input", function () {
     temperature = this.value;
 });
 
+function showPage(pageId) {
+    pageIds.forEach((page) => {
+        document.getElementById(page).classList.add("page-hidden");
+    });
+    document.getElementById(pageId).classList.remove("page-hidden");
+}
+
 function updatePressure(val) {
     let value = `${val}%`;
     document.getElementById("pressure-label").innerHTML = value;
     pressureValue = val;
+}
+
+function setSprayPattern(button) {
+    buttonIDs.forEach((bid) => {
+        document
+            .getElementById(bid)
+            .classList.remove("spray-select-button-active");
+    });
+
+    button.classList.add("spray-select-button-active");
+
+    // let icon = document.getElementById("spray-pattern-icon");
+    // icon.src = `/assets/spray-patterns/${button.value}.png`;
+
+    const icons = document.getElementsByClassName("spray-disp");
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].src = `/assets/spray-patterns/${button.value}.png`;
+    }
 }
