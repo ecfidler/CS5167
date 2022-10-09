7f60afbc5394889f127c56391526546e170ff550
Ethan Fidler <ethancf1999@gmail.com>
2022-10-09 16:45:50 -0400

    functionality to profiles page

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ projects for CS5167 - User Interfaces I
 
 ## TODO still
 
--   add music page
--   add profiles page
--   add timer page
+### prio
+
 -   make top bar work
+    -   timeout loop to update
 -   stop/start button functionality
+
+### stretch
+
+-   add music page
+-   add timer page
+    -   prompt when 1 minute remains (add this to the timeout loop)
--- main.js
+++ main.js
@@ -11,12 +11,36 @@ let buttonIDs = [
 
 let pageIds = ["main-page", "music-page", "faucet-page", "profiles-page"];
 
+let profiles = [
+    {
+        name: "Ethan",
+        temperature: 85,
+        pressure: 50,
+        sprayPattern: "shower",
+        totalLength: 630,
+    },
+    {
+        name: "Fidler",
+        temperature: 90,
+        pressure: 100,
+        sprayPattern: "mist",
+        totalLength: 900,
+    },
+    {
+        name: "Other",
+        temperature: 60,
+        pressure: 30,
+        sprayPattern: "shower",
+        totalLength: 300,
+    },
+];
+
 // MAIN DATA (will be refactored into a data structure)
 
 let temperatureValue = 85;
 let pressureValue = 50;
 let sprayPatternValue = "shower";
-let activeProfile = "Ethan Fidler";
+let activeProfile = "Ethan";
 let timeRemaining = "0";
 
 // move gradient on main page slider
@@ -52,6 +76,8 @@ temperatureInput.addEventListener("input", function () {
     // update
     document.getElementById("temp-label").innerHTML = `${this.value}°`;
     temperature = this.value;
+
+    temperatureValue = this.value;
 });
 
 function showPage(pageId) {
@@ -82,5 +108,139 @@ function setSprayPattern(button) {
     const icons = document.getElementsByClassName("spray-disp");
     for (let i = 0; i < icons.length; i++) {
         icons[i].src = `/assets/spray-patterns/${button.value}.png`;
+    }
+}
+
+function inputQuickOutput(el, unit) {
+    el.nextElementSibling.value = el.value + unit;
+}
+
+// Handling the loading and updating of profiles
+
+function loadProfiles() {
+    // profileNameInput
+    console.log("loading profiles...");
+    profiles.forEach((profile) => {
+        addProfileButton(profile);
+        console.log(`loaded ${profile.name}`);
+    });
+}
+
+function addProfileButton(profileData) {
+    const el = document.createElement("button");
+    el.innerText = profileData.name;
+    el.onclick = function () {
+        setActiveProfile(profileData.name); //`setActiveProfile(${profile.name})`;
+    };
+    el.classList.add("generic-button", "profile-select-button");
+    document.getElementById("profilesList").appendChild(el);
+}
+
+function setActiveProfile(profileName) {
+    let profileData = profiles.find((profile) => profile.name == profileName);
+
+    console.log(`Setting profile to: ${profileData}`);
+    setInputValues(profileData);
+}
+
+function getInputValues() {
+    // take in all of the values from the inputs and return an object
+    let name = document.getElementById("profileNameInput").value;
+
+    let temperature = document.getElementById("profileTemperatureInput").value;
+
+    let pressure = document.getElementById("profilePressureInput").value;
+
+    let sprayPattern = document.getElementById("selectSprayPattern").value;
+
+    let timeTemp =
+        document.getElementById("timeMinutesInput").value * 60 +
+        document.getElementById("timeSecondsInput").value;
+
+    let totalLength = 0;
+    if (timeTemp) {
+        let totalLength = timeTemp;
+    }
+
+    return {
+        name: name,
+        temperature: temperature,
+        pressure: pressure,
+        sprayPattern: sprayPattern,
+        totalLength: totalLength,
+    };
+}
+
+function setInputValues(profileObject) {
+    // replace all of the values in the inputs with values from an object
+    document.getElementById("profileNameInput").value = profileObject.name;
+
+    let tempInput = document.getElementById("profileTemperatureInput");
+    tempInput.value = profileObject.temperature;
+    inputQuickOutput(tempInput, "°");
+
+    let pressInput = document.getElementById("profilePressureInput");
+    pressInput.value = profileObject.pressure;
+    inputQuickOutput(pressInput, "%");
+
+    document.getElementById("selectSprayPattern").value =
+        profileObject.sprayPattern;
+
+    let mins = Math.floor(profileObject.totalLength / 60);
+    let seconds = profileObject.totalLength - mins * 60;
+    console.log(`${mins}:${seconds}`);
+    document.getElementById("timeMinutesInput").value = mins;
+    document.getElementById("timeSecondsInput").value = seconds;
+}
+
+function setGlobalValues(profileData) {
+    activeProfile = profileData.name;
+    timeRemaining = profileData.timeRemaining;
+    temperatureValue = profileData.temperature;
+    pressureValue = profileData.pressure;
+    sprayPatternValue = profileData.sprayPattern;
+
+    updateValues();
+}
+
+function updateValues() {
+    // then update all meters
+    let tempInput = document.getElementById("temp-input");
+
+    tempInput.value = temperatureValue;
+    tempInput.dispatchEvent(new Event("input"));
+
+    document.getElementById("pressure-input").value = pressureValue;
+    updatePressure(pressureValue);
+
+    setSprayPattern(
+        document.getElementById(`${sprayPatternValue}PatternButton`)
+    );
+
+    // the top bar will update automatically every second (profile and time remaining)
+}
+
+function newProfile() {
+    setInputValues({
+        name: "",
+        temperature: 85,
+        pressure: 50,
+        sprayPattern: "shower",
+        totalLength: 0,
+    });
+}
+
+function saveProfile() {
+    newData = getInputValues();
+
+    let isExistingIndex = profiles.findIndex(
+        (element) => element.name == newData.name
+    );
+
+    if (isExistingIndex != -1) {
+        profiles[isExistingIndex] = newData;
+    } else {
+        profiles.push(newData);
+        addProfileButton(newData);
     }
 }
